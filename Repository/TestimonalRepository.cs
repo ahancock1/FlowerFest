54922cd289aadfb285eca6bebd94a9b691376c81
Adam Hancock <a.hancock@hotmail.co.uk>
2018-02-25 13:32:09 +0000

    Implemented seeding for testimonal repository

--- Repository/TestimonalRepository.cs
+++ Repository/TestimonalRepository.cs
@@ -7,14 +7,39 @@
 
 namespace FlowerFest.Repository
 {
+    using System.IO;
     using Interfaces;
     using Models;
 
-    public class TestimonalRepository : Repository<TestimonalModel>, ITestimonalRepository
+    public class TestimonalRepository : BaseRepository<TestimonalModel>, ITestimonalRepository
     {
+        private readonly string _path;
+
         public TestimonalRepository(string path)
             : base(path)
         {
+            _path = path;
+
+            Seed();
+        }
+
+        private void Seed()
+        {
+            if (Directory.GetFiles(_path).Length > 0) return;
+
+            Create(new TestimonalModel
+            {
+                Author = "Tim Lloyd",
+                Content = "The FlowerFest 18 is a very exciting prospect for Christchurch and the fact that it is being centred around the Priory Church in conjunction with the Priory Music Festival will give this new event the exposure it deserves during it’s inaugural year. As a neighbourhood business we are proud to be supporters.",
+                Place = "Captain's Club Hotel"
+            });
+
+            Create(new TestimonalModel
+            {
+                Author = "Tom Le Mesurier",
+                Content = "We look forward to supporting FF18. Growing up in Christchurch it will be a fantastic to see it in bloom.",
+                Place = "Flora Direct"
+            });
         }
     }
 }
